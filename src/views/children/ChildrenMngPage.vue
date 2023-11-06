1bf40382ef6b21393d174216ae8d82b31245739f
FitnessDoge <930763179@qq.com>
2023-11-06 19:52:10 +0800

    children mng

--- src/views/children/ChildrenMngPage.vue
+++ src/views/children/ChildrenMngPage.vue
@@ -9,7 +9,8 @@ const childrenData = [
     gender: 'SECRET',
     usageDuration: 87,
     introduction: 'ipsum sunt nulla officia amet',
-    avatarURL: 'http://localhost:5173/src/assets/logo.png'
+    avatarURL: 'http://localhost:5173/src/assets/logo.png',
+    score: 100
   },
   {
     id: '22',
@@ -20,7 +21,8 @@ const childrenData = [
     gender: 'SECRET',
     usageDuration: 87,
     introduction: 'ipsum sunt nulla officia amet',
-    avatarURL: 'http://dummyimage.com/100x100'
+    avatarURL: 'http://localhost:5173/src/assets/logo.png',
+    score: 100
   },
   {
     id: '22',
@@ -31,7 +33,8 @@ const childrenData = [
     gender: 'SECRET',
     usageDuration: 87,
     introduction: 'ipsum sunt nulla officia amet',
-    avatarURL: 'http://localhost:5173/src/assets/logo.png'
+    avatarURL: 'http://localhost:5173/src/assets/logo.png',
+    score: 100
   },
   {
     id: '22',
@@ -42,7 +45,8 @@ const childrenData = [
     gender: 'SECRET',
     usageDuration: 87,
     introduction: 'ipsum sunt nulla officia amet',
-    avatarURL: 'http://dummyimage.com/100x100'
+    avatarURL: 'http://dummyimage.com/100x100',
+    score: 100
   },
   {
     id: '22',
@@ -53,7 +57,8 @@ const childrenData = [
     gender: 'SECRET',
     usageDuration: 87,
     introduction: 'ipsum sunt nulla officia amet',
-    avatarURL: 'http://dummyimage.com/100x100'
+    avatarURL: 'http://localhost:5173/src/assets/logo.png',
+    score: 100
   },
   {
     id: '22',
@@ -64,7 +69,32 @@ const childrenData = [
     gender: 'SECRET',
     usageDuration: 87,
     introduction: 'ipsum sunt nulla officia amet',
-    avatarURL: 'http://dummyimage.com/100x100'
+    avatarURL: 'http://dummyimage.com/100x100',
+    score: 100
+  },
+  {
+    id: '22',
+    username: '胡芳',
+    role: 'CHILD',
+    nickname: '邵娟',
+    password: 'sunt cillum labore non',
+    gender: 'SECRET',
+    usageDuration: 87,
+    introduction: 'ipsum sunt nulla officia amet',
+    avatarURL: 'http://dummyimage.com/100x100',
+    score: 100
+  },
+  {
+    id: '22',
+    username: '胡芳',
+    role: 'CHILD',
+    nickname: '邵娟',
+    password: 'sunt cillum labore non',
+    gender: 'SECRET',
+    usageDuration: 87,
+    introduction: 'ipsum sunt nulla officia amet',
+    avatarURL: 'http://dummyimage.com/100x100',
+    score: 100
   }
 ]
 const getGender = (genderStr: string) => {
@@ -95,32 +125,37 @@ const formatDuration = (sec: number) => {
 </script>
 
 <template>
-  <div class="table">
-    <el-table :data="childrenData" style="width: 100%" max-height="655px">
-      <el-table-column fixed prop="nickname" label="昵称" width="120" />
-      <el-table-column prop="username" label="用户名" width="150" />
-      <el-table-column prop="avatarURL" label="头像" width="150">
-        <template #default="{ row }">
-          <img class="avatar" :src="row.avatarURL" alt="avatar" />
-        </template>
-      </el-table-column>
-      <el-table-column prop="gender" label="性别" width="120">
-        <template #default="{ row }">
-          <el-tag>{{ getGender(row.gender) }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column prop="usageDuration" label="使用时长" width="120">
-        <template #default="{ row }">
-          {{ formatDuration(row.usageDuration) }}
-        </template>
-      </el-table-column>
-      <el-table-column fixed="right" label="操作" width="120">
-        <template #default>
-          <el-button link type="primary" size="small">Detail</el-button>
-          <el-button link type="primary" size="small">Edit</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+  <div>
+    <div class="search">条件搜索</div>
+    <div class="table">
+      <el-table :data="childrenData" style="width: 100%" max-height="655px">
+        <el-table-column type="index" label="序号" width="80" />
+        <el-table-column prop="nickname" label="昵称" />
+        <el-table-column prop="username" label="用户名" />
+        <el-table-column prop="avatarURL" label="头像">
+          <template #default="{ row }">
+            <img class="avatar" :src="row.avatarURL" alt="avatar" />
+          </template>
+        </el-table-column>
+        <el-table-column prop="gender" label="性别">
+          <template #default="{ row }">
+            <el-tag>{{ getGender(row.gender) }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="score" label="学习积分" />
+        <el-table-column prop="usageDuration" label="使用时长">
+          <template #default="{ row }">
+            {{ formatDuration(row.usageDuration) }}
+          </template>
+        </el-table-column>
+        <el-table-column fixed="right" label="操作">
+          <template #default>
+            <el-button link type="primary" size="small">Detail</el-button>
+            <el-button link type="primary" size="small">Edit</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
     <div class="pagination">
       <el-pagination
         background
@@ -133,9 +168,13 @@ const formatDuration = (sec: number) => {
 </template>
 
 <style scoped>
+.table {
+  display: flex;
+  justify-content: center;
+}
 .avatar {
-  width: 75px;
-  height: 75px;
+  width: 50px;
+  height: 50px;
   max-width: 100%;
   max-height: 100%;
 }
--- src/views/layout/LayoutContainer.vue
+++ src/views/layout/LayoutContainer.vue
@@ -1,41 +1,63 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container-layout">
+  <div>
     <el-container>
-      <el-aside width="200px">
-        <img class="logo" src="../../assets/logo.png" alt="logo" />
-        <hr />
-        <menu-bar></menu-bar>
-      </el-aside>
-      <el-container>
-        <el-header class="header"
-          ><h1 class="title">明光筑梦-后台管理系统</h1></el-header
-        >
-        <el-main>
-          <hr />
-          <router-view></router-view>
-        </el-main>
+      <el-header class="header">
+        <div class="flex">
+          <div class="flex">
+            <img class="logo" src="../../assets/logo.png" alt="logo" />
+            <div class="project-name">明光筑梦</div>
+          </div>
+          <h1 class="title">后台管理系统</h1>
+        </div>
+      </el-header>
 
-        <el-footer
-          ><hr />
-          <div>Footer</div>
-        </el-footer>
+      <el-container>
+        <el-aside width="200px">
+          <menu-bar></menu-bar>
+        </el-aside>
+
+        <el-container>
+          <el-main>
+            <router-view></router-view>
+          </el-main>
+          <el-footer class="footer"
+            >Copyright &copy;
+            <a href="https://github.com/nanoyou">nanoyou</a>
+          </el-footer>
+        </el-container>
       </el-container>
     </el-container>
   </div>
 </template>
 
 <style scoped>
+.footer {
+  margin-top: 10px;
+  text-align: center;
+}
+.project-name {
+  margin-left: 12px;
+  font-weight: 700;
+}
+.flex {
+  display: flex;
+  align-items: center;
+}
 .header {
-  height: 120px;
+  border-bottom: 1px solid #f4f4f4;
+  /* background-color: skyblue; */
+  /* mar */
+  /* height: 120px; */
 }
 .logo {
-  width: 150px;
-  height: 150px;
+  width: 50px;
+  height: 50px;
 }
 .title {
-  text-align: center;
-  font-size: 48px;
+  /* text-align: center; */
+  margin-left: 80px;
+  font-size: 22px;
 }
 </style>
