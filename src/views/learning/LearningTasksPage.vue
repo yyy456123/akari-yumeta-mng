07bf9dac07431833eecaf69134b07e1e5a8fc726
yyy456123 <20216827@stu.neu.edu.cn>
2023-11-10 17:21:33 +0800

    和你一样了

--- src/views/learning/LearningTasksPage.vue
+++ src/views/learning/LearningTasksPage.vue
@@ -3,34 +3,38 @@
     <div class="head">
       <div class="search">条件搜索</div>
       <div class="add-task">
-        <el-button type="primary" @click="showDrawer = true">新增任务</el-button>
+        <el-button type="primary" @click="handleEdit">新增任务</el-button>
       </div>
     </div>
 
     <!-- edit drawer -->
     <div class="drawer">
       <el-drawer
-        v-model="showDrawer"
-        title="添加任务"
-        :before-close="cancelCreateTask"
+        v-model="drawerVisible"
+        :title="drawerData.id ? '编辑任务' : '新增任务'"
+        :before-close="cancel"
         size="30%"
       >
-        <el-form :model="newTask" label-width="80px">
+        <el-form :model="drawerData" label-width="80px">
           <el-form-item label="任务名称" prop="title">
-            <el-input v-model="newTask.title"></el-input>
+            <el-input v-model="drawerData.title"></el-input>
           </el-form-item>
           <el-form-item label="标签" prop="tag">
-            <el-input v-model="newTask.tag"></el-input>
+            <el-input v-model="drawerData.tag"></el-input>
           </el-form-item>
           <el-form-item label="任务描述" prop="description">
-            <el-input v-model="newTask.description"></el-input>
+            <el-input v-model="drawerData.description"></el-input>
           </el-form-item>
           <el-form-item label="截止时间" prop="deadline">
-            <el-date-picker type="date" placeholder="选择日期" v-model="newTask.deadline"></el-date-picker>
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="drawerData.deadline"
+            ></el-date-picker>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="createTask">确定</el-button>
-            <el-button @click="cancelCreateTask">取消</el-button>
+            <el-button type="primary" @click="publish">确定</el-button>
+            <el-button @click="cancel">取消</el-button>
           </el-form-item>
         </el-form>
       </el-drawer>
@@ -51,40 +55,40 @@
         <el-table-column fixed="right" label="操作">
           <template #default="{ row }">
             <el-button link type="primary" size="small" @click="handleEdit(row)"
-            >编辑</el-button
+              >编辑</el-button
             >
             <el-button
               link
               type="danger"
               size="small"
               @click="deleteTask(row.id)"
-            >删除</el-button
+              >删除</el-button
             >
           </template>
         </el-table-column>
       </el-table>
     </div>
 
-    <div class="pagination">
-      <el-pagination
-        background
-        layout="prev, pager, next, jumper, total"
-        :total="totalTasks"
-        :page-size="pageSize"
-      />
-    </div>
+    <!--    <div class="pagination">-->
+    <!--      <el-pagination-->
+    <!--        background-->
+    <!--        layout="prev, pager, next, jumper, total"-->
+    <!--        :total="totalTasks"-->
+    <!--        :page-size="pageSize"-->
+    <!--      />-->
+    <!--    </div>-->
 
-    <div class="drawer">
-      <el-drawer
-        v-model="drawerVisible"
-        :title="drawerData.id ? '编辑任务' : '添加任务'"
-        :before-close="cancel"
-        direction="rtl"
-        size="50%"
-      >
-        <!-- 表单元素和提交按钮 -->
-      </el-drawer>
-    </div>
+    <!--    <div class="drawer">-->
+    <!--      <el-drawer-->
+    <!--        v-model="drawerVisible"-->
+    <!--        :title="drawerData.id ? '编辑任务' : '添加任务'"-->
+    <!--        :before-close="cancel"-->
+    <!--        direction="rtl"-->
+    <!--        size="50%"-->
+    <!--      >-->
+    <!--        &lt;!&ndash; 表单元素和提交按钮 &ndash;&gt;-->
+    <!--      </el-drawer>-->
+    <!--    </div>-->
   </div>
 </template>
 
@@ -92,7 +96,8 @@
 import { ref } from 'vue'
 import { ElButton, ElTable, ElTag, ElPagination, ElDrawer } from 'element-plus'
 
-const taskData = ref([
+const taskData = ref({})
+taskData.value = [
   {
     id: '1',
     title: '学习Vue3',
@@ -100,16 +105,34 @@ const taskData = ref([
     description: '学习Vue3框架及其生态工具',
     status: '进行中',
     deadline: '2023-12-31'
-  },
+  }
   // 其他任务数据...
-])
+]
 
+const editor = ref()
 const drawerVisible = ref(false)
 const drawerData = ref({})
 // ...其他变量和函数
 
-const handleEdit = (row) => {
-  // 编辑任务的逻辑
+const handleEdit = (row = {}) => {
+  drawerData.value = row
+  drawerVisible.value = true
+}
+
+const clearDrawer = () => {
+  drawerData.value = {}
+  editor.value.setHTML('')
+  drawerVisible.value = false
+}
+
+const publish = () => {
+  // TODO:send request, add or update
+  if (!drawerData.value.id) {
+    drawerData.value.id = 100
+  }
+  taskData.value.push(drawerData.value)
+  clearDrawer()
+  drawerVisible.value = false
 }
 
 const deleteTask = (id) => {
@@ -118,30 +141,12 @@ const deleteTask = (id) => {
 
 const cancel = () => {
   // 取消编辑任务的逻辑
+  drawerData.value = {}
+  drawerVisible.value = false
 }
 
 const totalTasks = 100 // 总任务数
-const pageSize = 10 // 每页显示的任务数
-
-const showDrawer = ref(false)
-const newTask = ref({
-  title: '',
-  tag: '',
-  description: '',
-  deadline: ''
-})
-
-const createTask = () => {
-  // 提交新任务的逻辑
-  console.log('提交新任务', newTask.value)
-  showDrawer.value = false // 提交后关闭抽屉
-}
-
-const cancelCreateTask = () => {
-  // 取消创建任务的逻辑
-  newTask.value = { title: '', tag: '', description: '', deadline: '' } // 清空表单数据
-  showDrawer.value = false
-}
+const pageSize = 10 // 每页显示的任务数createTask
 </script>
 
 <style scoped>
